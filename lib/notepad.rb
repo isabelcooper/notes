f69b1f8e69d6e308a69563e357d05caff8e8b1e7
isabelcooper <isabel@triptease.com>
2019-01-13 09:31:43 +0000

    get user inout working in feature test but rspec is still wrong

--- lib/notepad.rb
+++ lib/notepad.rb
@@ -9,4 +9,14 @@ class Notepad
   def add_to_list(title, body)
     @all_notes << {title => body}
   end
+
+  def get_user_input
+    puts "Enter title"
+    title = gets.chomp
+    puts "Enter body"
+    body = gets.chomp
+    add_to_list(title, body)
+  end
+
+
 end
--- spec/notepad_spec.rb
+++ spec/notepad_spec.rb
@@ -8,8 +8,14 @@ describe Notepad do
   it { is_expected.to respond_to(:add_to_list).with(2).arguments }
 
   it 'should be able to store a list of notes' do
-
     @notepad.add_to_list("Title","Body")
     expect(@notepad.all_notes).to eq [{"Title" => "Body"}]
   end
+
+#   it 'should prompt user to enter notes' do
+#     allow(STDIN).to receive(:gets) { ‘TestTitle’ }
+#     allow(@notepad.get_user_input).to receive(:gets).and_return(TestTitle','TestBody')
+#     expect(@notepad.get_user_input).to eq [{'TestTitle' => 'TestBody'}]
+#     expect(@notepad.all_notes).to eq [{'TestTitle' => 'TestBody'}]
+#   end
 end
